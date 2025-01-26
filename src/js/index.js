6ada8637d5675192bce4c2a2530c14bd16ad6795
Rylder-Rodrygo <rylderrodrygo031@gmail.com>
2025-01-26 13:46:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+const btnVoltar = document.getElementById("btn-voltar")
+
+btnAvancar.addEventListener("click", function () {
+    
+    if (cartaoAtual === cartoes.length - 1) return;
+    
+    esconderCartaoSelecionado();
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+
+    if (cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado()
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual)
+});
+
+function mostrarCartao(cartaoAtual) {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
